ee85ee93528ac96d1838954dd050352ae467e46c
dave <dave@avanthay.ch>
2013-09-28 12:26:08 +0200

    code written for exercice MdpUebungSelbstaendigJunitTestsSchreiben

--- src/main/java/ch/dave/junit/exercice/FirstJUnit.java
+++ src/main/java/ch/dave/junit/exercice/FirstJUnit.java
@@ -1,5 +1,33 @@
 package ch.dave.junit.exercice;
 
-public class FirstJUnit {
+public class FirstJUnit implements IMdpJunitDemo {
+
+	public boolean isEmpty(String str) {
+		if (str == null || str.length() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public String capitalize(String str) {
+		String begin = str.substring(0, 1);
+		String end = str.substring(1);
+		return begin.toUpperCase() + end.toLowerCase();
+	}
+
+	public String reverse(String str) throws NullPointerException {
+		if (str == null){
+			throw new NullPointerException("parameter is null");
+		}
+		return new StringBuilder(str).reverse().toString();
+	}
+
+	public String join(String... strings) {
+		String str = strings[0];
+		for(int i = 1; i < strings.length; i++){
+			str = str + " " + strings[i];
+		}
+		return str;
+	}
 
 }
